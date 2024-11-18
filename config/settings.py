28d8ea2459d1d9fe62d69817083ad34e2c3df764
cleoivvy <claraojobo@gmail.com>
2024-11-18 14:45:15 +0100

    fix: just work

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['weather-server-39vm.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','weather-server-39vm.onrender.com']
 
 
 # Application definition
